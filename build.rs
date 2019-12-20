2c7aabaa316f37455d3c4e0f575a5c28a36ca8dc
William D. Jones <thor0505@comcast.net>
2019-12-20 01:22:54 -0500

    If using the device feature, make sure peripheral interrupt handler symbols are PROVIDEd.

--- build.rs
+++ build.rs
@@ -10,10 +10,28 @@ fn main() {
     }
 
     // Put the linker script somewhere the linker can find it
-    File::create(out_dir.join("link.x"))
-        .unwrap()
-        .write_all(include_bytes!("link.x"))
-        .unwrap();
+    let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
+    let link_x = include_bytes!("link.x.in");
+    if env::var_os("CARGO_FEATURE_DEVICE").is_some() {
+        let mut f = File::create(out.join("link.x")).unwrap();
+
+        f.write_all(link_x).unwrap();
+
+        // *IMPORTANT*: The weak aliases (i.e. `PROVIDED`) must come *after* `EXTERN(__INTERRUPTS)`.
+        // Otherwise the linker will ignore user defined interrupts and always populate the table
+        // with the weak aliases.
+        writeln!(
+            f,
+            r#"
+/* Provides weak aliases (cf. PROVIDED) for device specific interrupt handlers */
+/* This will usually be provided by a device crate generated using svd2rust (see `device.x`) */
+INCLUDE device.x"#
+        ).unwrap();
+    } else {
+        let mut f = File::create(out.join("link.x")).unwrap();
+        f.write_all(link_x).unwrap();
+    };
+
     println!("cargo:rustc-link-search={}", out_dir.display());
 
     println!("cargo:rerun-if-changed=build.rs");
